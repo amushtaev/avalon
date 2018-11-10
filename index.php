fbc9d3658b3f5276a2896cb5c3b8ce2a0a52b55b
amushtaev <ms@adesign.dp.ua>
2018-11-10 23:02:02 +0200

    Revert "add reviews"

--- index.php
+++ index.php
@@ -194,46 +194,7 @@ endif;
                 <div id="b9_button1">
                     <button class="center_button scroll red_button" href="#block5" top="250"><span>Попробовать бесплатно</span></button>
                 </div>
-                <div class="card">
-                    <div class="card_name">Разовое посещение</div>
-                    <div class="card_service" style="display: block; width: 100%;">
-                        <object style="max-width: 82%;" type="image/svg+xml" data="/wp-content/themes/avalon/img/1_3.svg">
-                    </div>
-                    <div class="card_cost">60</div>
-                </div>
-                <div class="card">
-                    <div class="card_name">Абонемент<br/>Утро-День</div>
-                    <div class="card_service">
-        <span>
-            <p><strong>посещение</strong></p>
-            <p>пн-пт 7.00-22.00</p>
-            <p>сб 10.00-20.</p>
-            <p>вс 11.00-18.00</p>
-        </span>
-                    </div>
-                    <div class="card_cost">900</div>
-                </div>
-                <div class="card">
-                    <div class="card_name">Абонемент <br/>на 3 месяца</div>
-                    <div class="card_service" style="display: block; width: 100%;">
-        <span>
-            <p><strong>+15 дней заморозки</strong></p>
-            <object style="max-width: 78%;" type="image/svg+xml" data="/wp-content/themes/avalon/img/1_4.svg"></object>
-        </span>
-                    </div>
-                    <div class="card_cost">900</div>
-                </div>
-                <div class="card">
-                    <div class="card_service" style="top: 30px;">
-        <span>
-            <p>Доплата за тренера разовая <strong>130грн</strong></p>
-            <p>Доплата за 10 занятий/месяц с тренером <strong>1170грн</strong></p>
-            <p>Программа тренировки  или питания <strong>100грн</strong></p>
-            <p>Аренда шкафчика <strong>50грн</strong></p>
-            <p>Поломка или потеря ключа <strong>50грн</strong></p>
-        </span>
-                    </div>
-                </div>
+                    <?php get_template_part( 'content', 'card' ); ?>
                 <div id="cards_buyed">
                     <span style="color:#7f0082;">Первое посещение - БЕСПЛАТНО</span>
                 </div>
